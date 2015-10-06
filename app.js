1a40e6eb741cc5861d33ba0708877b191d41bdb2
Juan David Hermoso <juandavidhermoso@gmail.com>
2015-10-06 22:21:27 +0200

    Trying in port 5000

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    io = require('socket.io').listen(server),
+    port = Number(process.env.PORT || 5000);
 
-server.listen();
+server.listen(port, console.log.bind(console, 'Node running in http://localhost:' + port));
 app.use(express.static(__dirname+'/public'));
 app.get('/',function(req,res){
 
