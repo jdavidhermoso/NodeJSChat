26b24b0b246721498b068e036974de1e24a03dfb
Juan David Hermoso <juandavidhermoso@gmail.com>
2015-10-07 22:44:24 +0200

    ...

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express'),
     WebSocketServer = require("ws").Server,
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
     port = Number(process.env.PORT || 5000);
 
 server.listen(port);
+
 app.use(express.static(__dirname+'/public'));
 
 app.get('/',function(req,res){
@@ -14,10 +14,6 @@ app.get('/',function(req,res){
 });
 
 var wss = new WebSocketServer({server:server});
-console.log('Websocket server was created!');
-
-
-
 
 wss.on('connection', function(socket){
     //Every time someone connects to the chat, a socket is created.
